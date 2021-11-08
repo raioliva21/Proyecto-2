28b9822d745eaeb59479f8bef8ac600498490d1a
raioliva21 <89752816+raioliva21@users.noreply.github.com>
2021-11-08 17:24:18 -0300

    Update creacion_Json.py

--- creacion_Json.py
+++ creacion_Json.py
@@ -1,39 +1,94 @@
 import json
 
-def procesar_archivo(archivo = None):
+#def procesar_datos_paises(datos_paises):
+
+def eliminar_datos_inutiles(archivo = None):
+
+    archivo = archivo.strip().split(",")
+    archivo = archivo[0:8]
+    archivo.pop(1)
+    archivo.pop(5)
+    
+    return archivo
+
+def procesar_archivos(archivo, archivo_2):
+
+    print("\n")
 
     total_lineas = len(archivo) - 1
+    variables_descriptivas = archivo[0]
     datos_paises = archivo[1:total_lineas]
 
-    # se procesa linea a linea el archivo
-    # for indice in range(0, len(archivo)):
-    for indice in range(0,6):
-        if indice == 0:
-            print("Linea correspondiente a los indicadores de datos")
-            print(archivo[0])
-            descripcion_datos = archivo[0]
-            descripcion_datos = descripcion_datos.strip().split(",")
-        else:
-            datos_paises[indice].strip().split(",")
-            for subindice in range(0, len(datos_paises[indice])):
-                if datos_paises[indice][subindice] == '':
-                    #print(f"Se elimina valor de posicion {subindice} en linea {archivo[indice]}")
-                    datos_paises[indice].pop(subindice)
-                    # se debe concatenar lineas referentes a mismo pais
+    variables_descriptivas = eliminar_datos_inutiles(variables_descriptivas)
+    print(variables_descriptivas)
+    print("\n")
+
+
+    data_paises = {}
+    data_paises['paises'] = []
+    print(type(data_paises))
+    
+
+    for linea in range(0,1):
+
+        datos_de_pais = eliminar_datos_inutiles(datos_paises[linea])
+        print("'dato_pais_' es de tipo : ", end = "")
+        print(type(datos_de_pais))
+        pais = datos_de_pais[0]
+        fecha = datos_de_pais[1] 
+        fecha = fecha.strip().split("-")
+        anio = fecha[0]
+        mes = fecha[1]
+        dia = fecha[2]
+
+        print(type(datos_de_pais[2]))
+
+        for indice in range(2,5):
+            if datos_de_pais[indice] == '0.0':
+                datos_de_pais[indice] = 'no existe registro para la fecha'
+            else:
+                pass
+            
+
+        num_absoluto_inmunizaciones = datos_de_pais[2]
+        num_total_vacunados = datos_de_pais[3]
+        num_total_personas_con_esquema_completo = datos_de_pais[4]
+
+        lista_de_datos = [num_absoluto_inmunizaciones, num_total_vacunados , num_total_personas_con_esquema_completo]
+
+        data_paises.update ({f"{pais}-{anio}-{mes}-{dia}" : lista_de_datos})
+
+        #print("se imprimen los datos de pais")
+        #print(data_paises[f"{pais}-{anio}-{mes}-{dia}"][0])
+
+
 
 def abrir_archivo(nombre_archivo = None):
 
     try:
         with open(nombre_archivo) as archivo:
             datos_archivo = archivo.readlines()
+            print(f"{nombre_archivo} ha sido abierto")
             return datos_archivo
-            #print(f"{nombre_archivo} ha sido abierto")
-            #return archivo
     except FileNotFoundError:
         print("Error, el archivo no fue encontrado")
-        #quit()
+        quit()
+
 
 def main():
+    
+    archivo_1 = "country_vaccinations.csv"
+    archivo_1 = "/home/raimundoosf/Escritorio/Python/En clases/Proyecto N°2/country_vaccinations.csv"
+    archivo_2 = "/home/raimundoosf/Escritorio/Python/En clases/Proyecto N°2/country_vaccinations_by_manufacturer.csv"
+    datos_archivo_1 = abrir_archivo(archivo_1)
+    datos_archivo_2 = abrir_archivo(archivo_2)
+    procesar_archivos(datos_archivo_1, datos_archivo_2)
+    #datos_archivo_2 = procesar_archivo(datos_archivo_2)
+    #crear_json(archivo_uno)
+
+
+if __name__ == "__main__":
+    main()
     
     archivo_1 = "country_vaccinations.csv"
     archivo_2 = "country_vaccinations_by_manufacturer.csv"
