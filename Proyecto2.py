2e9fdab1352252abb49bc34e75ca13194d289b53
raioliva21 <89752816+raioliva21@users.noreply.github.com>
2021-11-11 15:17:28 -0300

    Update Proyecto2.py

--- Proyecto2.py
+++ Proyecto2.py
@@ -36,6 +36,7 @@ def ingreso_de_datos_a_consultar(solicitar_comparacion, numero_pais_a_consultar)
         try:
             if solicitar_comparacion == False:
                 pais = input("Seleccione el pais a mostrar: ")
+                print("\n")
             else:
                 if numero_pais_a_consultar == 1:
                     pais = input("Ingrese pais a comparar: ")
@@ -49,10 +50,9 @@ def ingreso_de_datos_a_consultar(solicitar_comparacion, numero_pais_a_consultar)
             print("Error, valor ingresado no corresponde a cadena de caracteres (palabra(s))")
             continue
     
-    if numero_pais_a_consultar != 1:
+    if numero_pais_a_consultar == 1 and solicitar_comparacion == True or numero_pais_a_consultar == 2:
         print("Se solicitara fecha especifica a consultar.")
         while True:
-            
             anio = input("Año: ")
             mes = input("Mes: ")
             dia = input("Dia: ")
@@ -70,22 +70,42 @@ def ingreso_de_datos_a_consultar(solicitar_comparacion, numero_pais_a_consultar)
 
     return pais, anio, mes, dia
 
+def convertir_a_str(dato = None):
+
+	dato = str(dato)
+	if len(dato) == 1:
+		dato = "0" + dato
+	else:
+		pass
+    
+	return dato
+
+def evaluar_fecha(anio, mes, dia, registro_encontrado):
+
+    mes = int(mes)
+    dia = int(dia)
+
+    if anio == 2020 and mes == 12 and dia == 27 or anio == 2020 and mes == 12 and registro_encontrado == True:
+        anio = 2021
+        mes = 0
+        dia = 32
+
+    return anio, mes, dia
 
 def imprimir_data_pais(data_vacunacion, pais, anio, mes, dia, decision_usuario):
 
     #print(f"{pais}-{anio}-{mes}-{dia}")
 
     if f"{pais}-{anio}-{mes}-{dia}" in data_vacunacion:
-        print(f"{pais}:")
         if decision_usuario == 1:
             print(f"Anio: {anio}")
-            print(f"Mes: {mes}")
-            print(f"dia: {dia}")
+            print(f"Mes: {int(mes)}")
         else:
+            print(f"{pais}:")
             print(f"Numero de personas vacunadas aquel dia: {data_vacunacion[f'{pais}-{anio}-{mes}-{dia}'][3]}")
         print(f"Numero absoluto de inmunizaciones: {data_vacunacion[f'{pais}-{anio}-{mes}-{dia}'][0]}")
         print(f"Numero total de personas vacunadas: {data_vacunacion[f'{pais}-{anio}-{mes}-{dia}'][1]}")
-        print(f"Numero total de personas con el esquema completo: {data_vacunacion[f'{pais}-{anio}-{mes}-{dia}'][2]}")
+        print(f"Numero total de personas con el esquema completo: {data_vacunacion[f'{pais}-{anio}-{mes}-{dia}'][2]}\n")
         #print(f"Vacuna utilizada: {}")
         #print(f"Merma de vacunas: {}")
     else:
@@ -96,26 +116,33 @@ def procesar_decision_usuario(decision_usuario, data_vacunacion):
 
     if decision_usuario == 1:
 
-        while True:
-            try:
-                pais = input("Seleccione el pais a mostrar: ")
-                pais.capitalize()
-                print(pais)
-                if type(pais) == str:
-                    pass
-            except ValueError:
-                print("Error, valor ingresado no corresponde a cadena de caracteres (palabra(s))")
-                continue
+        solicitar_comparacion = False
+        numero_pais_a_consultar = None
+
+        pais, anio, mes, dia = ingreso_de_datos_a_consultar(solicitar_comparacion, numero_pais_a_consultar)
         
-    """
-        #for anio in range(2020,2022):
-            for mes in range(1,13):
-                for dia in range(1,32):
-                    if data_vacunacion[f"{pais}-{anio}-{mes}-{dia}"] not in data_vacunacion:
-                        continue
-                    else: 
-                        #registro_final_de_mes = {pais}-{anio}-{mes}-{dia}
-"""
+        anio = 2020
+        mes = 0
+        registro_encontrado = None
+
+        print(f"{pais}")
+        while mes != 13:
+            mes += 1
+            for dia in range(32,26,-1):
+                mes = convertir_a_str(mes)
+                dia = convertir_a_str(dia)
+                if f"{pais}-{anio}-{mes}-{dia}" in data_vacunacion:
+                    if data_vacunacion[f"{pais}-{anio}-{mes}-{dia}"][0] != 'no existe registro para la fecha':
+                        imprimir_data_pais(data_vacunacion, pais, anio, mes, dia, decision_usuario)
+                        registro_encontrado = True
+                        anio, mes, dia = evaluar_fecha(anio, mes, dia, registro_encontrado)
+                        break
+                    else:
+                        registro_encontrado = False
+                        anio, mes, dia = evaluar_fecha(anio, mes, dia, registro_encontrado)
+                else:
+                    registro_encontrado = False    
+                    anio, mes, dia = evaluar_fecha(anio, mes, dia, registro_encontrado)
 
     if decision_usuario == 2:
 
