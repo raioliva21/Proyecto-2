b914babdf2f13f14a46b9f538fc50ace279ab02f
raioliva21 <89752816+raioliva21@users.noreply.github.com>
2021-11-11 20:20:05 -0300

    Update creacion_Json.py

--- creacion_Json.py
+++ creacion_Json.py
@@ -1,5 +1,6 @@
 import json
 
+# modulo que abre archivos de extension csv
 def abrir_archivo(nombre_archivo = None):
 
     try:
@@ -11,28 +12,38 @@ def abrir_archivo(nombre_archivo = None):
         print("Error, el archivo no fue encontrado")
         quit()
 
+# se modifica determina linea de archivo
+def modificar_linea_archivo(linea = None):
 
-def modificar_archivo(archivo = None):
-
-    archivo = archivo.strip().split(",")
-    archivo = archivo[0:8]
-    archivo.pop(1)
-    archivo.pop(5)
+    # se pasa a lista
+    linea = linea.strip().split(",")
+    # se dejan elementos de lista 'linea' que son requeridos, eliminando aquellos que no son utiles
+    linea = linea[0:8]
+    linea.pop(1)
+    linea.pop(5)
     
-    return archivo
+    # se retorna linea determinada de archivo ya modificada
+    return linea
 
+# modulo que evalua si existe correlacion de datos entre determinada linea de archivo 1 con linea de archivo 2
 def procesar_datos_archivo2 (archivo_2, pais, fecha):
 
     fabricante_vacuna = []
     numero_de_vacunas = []
     vacunas_utilizadas = ""
 
-    # crear diccionario de archivo_2
-    # tal que llaves sean = '{pais}-{fecha} : fabricante_vacuna'
+    # linea[0] de archivo 2 corresponde a pais determiando
+    # linea[1] de archivo 2 corresponde a fecha determinada de tal pais
+    # linea[2] de archivo 2 corresponde al numero de vacunas de determinada fabrica (enunciada en 'linea[3])
+    # linea[3] de archivo 2 corresponde al fabricante de vacuna respectivo 
 
+    # se itinera archivo 2
     for linea in archivo_2:
+        # se pasa linea de archivo 2 a lista 
         linea = linea.strip().split(",")
+        # se datos pais y fecha, pertenecientes a linea de archivo 1, se encuentran tambien en archivo 2
         if linea[0] == pais and linea[1] == fecha:
+            # si el numero de vacunas suministradas de determido fabricante no es igual a cero 
             if linea[3] != "0":
                 fabricante_vacuna.append(linea[2])
                 numero_de_vacunas.append(linea[3])
@@ -44,31 +55,35 @@ def procesar_datos_archivo2 (archivo_2, pais, fecha):
     for indice in range (0, len(fabricante_vacuna)):
         vacunas_utilizadas += f"{fabricante_vacuna[indice]}({numero_de_vacunas[indice]}) "
     
+    # en acaso de que no haya hubiese informacion de la vacuna utilizada y fabricante
     if fabricante_vacuna == [] and numero_de_vacunas == []:
         vacunas_utilizadas = "Informacion no disponible"
 
-    print(vacunas_utilizadas)
-
     return vacunas_utilizadas
 
-
+# modulo que se encarga de procesar los archivos csv, obteniendo la informacion util de tales archivos
 def procesar_archivos(archivo, archivo_2):
 
     total_lineas = len(archivo)
     datos_paises = archivo[1:total_lineas]
     total_lineas -= 1
 
+    # se crea diccionario que contendra data de paises y proceso de vacunacion respectivo
     data_paises = {}
 
     for linea in range(0,total_lineas):
 
-        datos_de_pais = modificar_archivo(datos_paises[linea])
+        # numero absoluto de inmunizaciones = datos_de_pais[2]
+        # numero total de vacunados = datos_de_pais[3]
+        # numero total de personas con esquema completo = datos_de_pais[4]
+        # numero de vacunados ese dia = datos_de_pais[5]
+
+        datos_de_pais = modificar_linea_archivo(datos_paises[linea])
         pais = datos_de_pais[0]
         fecha = datos_de_pais[1] 
         vacunas_utilizadas = procesar_datos_archivo2(archivo_2, pais, fecha)
 
-
-        for indice in range(2,5):
+        for indice in range(2,6):
             if datos_de_pais[indice] == '':
                 if indice != 4:
                     datos_de_pais[indice] = 'no existe registro para la fecha'
@@ -79,12 +94,11 @@ def procesar_archivos(archivo, archivo_2):
             datos_de_pais[indice] = datos_de_pais[indice].split(".")
             datos_de_pais[indice] = datos_de_pais[indice][0]
 
-        # numero absoluto de inmunizaciones = datos_de_pais[2]
-        # numero total de vacunados = datos_de_pais[3]
-        # numero total de personas con esquema completo = datos_de_pais[4]
-        # numero de vacunados ese dia = datos_de_pais[5]
+        """
+        no se logro encontrar informacion para determinar la merma de vacunas
+        """
 
-        lista_de_datos = [datos_de_pais[2], datos_de_pais[3], datos_de_pais[4], datos_de_pais[5], vacunas_utilizadas ]
+        lista_de_datos = [datos_de_pais[2], datos_de_pais[3], datos_de_pais[4], vacunas_utilizadas, datos_de_pais[5]]
 
         data_paises.update ({f"{pais}-{fecha}" : lista_de_datos})
 
@@ -100,6 +114,7 @@ def main():
     datos_archivo_1 = abrir_archivo(archivo_1)
     datos_archivo_2 = abrir_archivo(archivo_2)
     data_paises = procesar_archivos(datos_archivo_1, datos_archivo_2)
+    # se crea archivo json que contendra informacion solicitada del proceso de vacunacion en paises
     with open("data_vacunacion.json", "w") as data:
         json.dump(data_paises, data, indent=4)
 
